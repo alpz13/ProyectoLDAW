c546aef8a6266d5ecf528a275051588d0ba56289
Miguel Flores Gaspar <xxfinalmikexx@gmail.com>
2014-09-29 15:25:20 -0500

    Reglas de verificación para el registro completas

--- application/views/registrarUsuariosView.php
+++ application/views/registrarUsuariosView.php
@@ -17,38 +17,44 @@
         </div>
             <div class="principalAreaP dark">
                 <?php echo form_open('usuariosController/registraUsuario'); ?>
-                    <table>
+                    <table class="tRegistro">
                         <tr>
                             <td>Nombre:</td>
                             <td><input type='text' name='nombre' value="<?php echo set_value('nombre');?>"</td>
+                            <td class="error"><?php echo form_error('nombre'); ?></td>
                         </tr>
                         <tr>
                             <td>Apellido Paterno:</td>
                             <td><input type='text' name='apellidoP' value="<?php echo set_value('apellidoP');?>"</td>
+                            <td class="error"><?php echo form_error('apellidoP'); ?></td>
                         </tr>
                         <tr>
                             <td>Apellido Materno:</td>
                             <td><input type='text' name='apellidoM' value="<?php echo set_value('apellidoM');?>"</td>
+                            <td class="error"><?php echo form_error('apellidoM'); ?></td>
                         </tr>
                         <tr>
                             <td>Contraseña:</td>
                             <td><input type='password' name='pass' value="<?php echo set_value('pass');?>"</td>
+                            <td class="error"><?php echo form_error('pass'); ?></td>
                         </tr>
                         <tr>
                             <td>Repita su contraseña:</td>
                             <td><input type='password' name='passCon' value="<?php echo set_value('passCon');?>"</td>
+                            <td class="error"><?php echo form_error('passCon'); ?></td>
                         </tr>
                         <tr>
                             <td>Correo:</td>
                             <td><input type='text' name='mail' value="<?php echo set_value('mail');?>"</td>
+                            <td class="error"><?php echo form_error('mail'); ?></td>
                         </tr>
                         <tr>
                             <td></td>
+                            <br/><br/>
                             <td><input type='submit' value='Registrar'/></td>
                         </tr>
                     </table>
                 <?php echo form_close(); ?>
-                <?php echo validation_errors(); ?>
                 <?php 
                     if(isset($error)) {
                             echo "<div class='error'>".$error."</div>";
