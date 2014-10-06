46805233435fd9d8726e52f7740ccf1129c2c99d
alpz13 <alpz13_2@hotmail.com>
2014-10-06 15:47:45 -0500

    icono de pestaña agregado

--- application/views/header.php
+++ application/views/header.php
@@ -6,6 +6,6 @@
         <script type="text/javascript" src="../../javascript/jquery.js"></script>
         <script type="text/javascript" src="../../javascript/principal.js"></script>
         <title>Job Scope</title>
-        
+        <link type="image/x-icon" href="images/websiteico.ico" rel="shortcut icon"/>
         <link href="<?php echo base_url(); ?>/css/styles.css" rel="stylesheet" type="text/css" />
     </head>
