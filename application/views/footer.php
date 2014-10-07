a5c0ea9eecdc23957f30c959172d59fb8e718b28
neronplus <neronplus@gmail.com>
2014-10-07 18:25:37 -0500

    Modified views/footer.php, views/headAdmin.php and views/header.php
    to implement new look to the interface.

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,7 +1,18 @@
+
+
+<!-- Version anterior
 <footer>
     <div>
         Copyright 2014 &COPY; &REG;
         <br/>
         Todos los derechos reservados
     </div>
+</footer>
+
+-->
+<footer>
+      <div id="Bottom">
+         <p class="down">Copyright © 2014, <a href="http://www.jobscope.com">
+		 Design by: Job Scope Team</a></p>
+      </div>
 </footer>
--- application/views/header.php
+++ application/views/header.php
@@ -1,3 +1,4 @@
+<!-- Version anterior
 <!DOCTYPE html>
 <html>
     <head>
@@ -8,4 +9,19 @@
         <title>Job Scope</title>
         <link type="image/x-icon" href="images/websiteico.ico" rel="shortcut icon"/>
         <link href="<?php echo base_url(); ?>/css/styles.css" rel="stylesheet" type="text/css" />
+    </head>
+    -->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta  http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script type="text/javascript" src="../../javascript/jquery.js"></script>
+        <script type="text/javascript" src="../../javascript/principal.js"></script>
+        <title>Job Scope</title>
+        <link type="image/x-icon" href="images/websiteico.ico" rel="shortcut icon"/>
+        <link href="<?php echo base_url(); ?>/css/styles.css" rel="stylesheet" type="text/css" />
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>default.css" title="default">
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>css/base.css" >
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>style.css" >
     </head>
